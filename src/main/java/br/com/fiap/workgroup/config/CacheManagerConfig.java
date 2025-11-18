be7e3ed2937506a19622d4db96930753108accfc
joaobroggine <joaovitorbrogginelopes@gmail.com>
2025-11-18 18:48:01 -0300

    Corrigindo imports do CacheManagerConfig.java

--- src/main/java/br/com/fiap/workgroup/config/CacheManagerConfig.java
+++ src/main/java/br/com/fiap/workgroup/config/CacheManagerConfig.java
@@ -1,8 +1,18 @@
 package br.com.fiap.workgroup.config;
 
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+
 @Configuration
 public class CacheManagerConfig {
 
+    @SuppressWarnings("null")
     @Bean
     public CacheManager cacheManager() {
         CaffeineCacheManager cacheManager = new CaffeineCacheManager();
