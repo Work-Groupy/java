ecfefac6118fa961320a8ad0a791d867f1e781e6
joaobroggine <joaovitorbrogginelopes@gmail.com>
2025-11-18 18:48:43 -0300

    Corrigindo imports do UserService.java

--- src/main/java/br/com/fiap/workgroup/services/UserService.java
+++ src/main/java/br/com/fiap/workgroup/services/UserService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import br.com.fiap.workgroup.models.User;
